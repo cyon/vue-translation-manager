f62ed435f9bb8988ae521a2c28d16c7e8fbd30bd
Max Gfeller <mg@cyon.ch>
2018-11-22 11:22:04 +0100

    Try with different spaces

--- test/data/test-1/src/components/Test.vue
+++ test/data/test-1/src/components/Test.vue
@@ -5,7 +5,7 @@
       <li v-for="user in users" :key="user.id">
         <b>Name: </b> {{user.name}}<br>
         <b>Birthday: </b> {{user.birthday}}<br>
-        <b>Hello text: {{ user.male ? 'Mr' : 'Mrs' }} {{ user.name }}</b><br>
+        <b>Hello text: {{ user.male ? 'Mr' : 'Mrs' }} {{user.name}}</b><br>
         <b>{{ user.name }} Birthday: {{ user.birthday}}</b><br>
       </li>
     </ul>
